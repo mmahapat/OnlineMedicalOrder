c14fce7733b5466e238fa5100b9b6813d6871f89
mmahapat <mmahapat@asu.edu>
2021-06-14 02:38:09 -0700

    feat: Skeleton Check-In

---
+++ src/main/java/com/medical/home/MedicalOrderManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medical.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalOrderManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalOrderManagementServiceApplication.class, args);
+    }
+
+}
